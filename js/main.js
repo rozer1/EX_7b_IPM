4d3629c33e3d9e8959ed34b27ce104c64b112d28
Tobiasz <209458@edu.p.lodz.pl>
2021-05-10 11:00:42 +0200

    transfer scrpits to main.js

--- js/main.js
+++ js/main.js
@@ -235,3 +235,43 @@ function GetRandom(){
 	var myElement8 = document.getElementById("id_card")
         myElement8.value = bigAlphabet[Math.floor(Math.random() * bigAlphabet.length)] + bigAlphabet[Math.floor(Math.random() * bigAlphabet.length)] + bigAlphabet[Math.floor(Math.random() * bigAlphabet.length)] + Math.floor(Math.random() * 899999 + 100000).toString();
 }
+
+			function getContactFromForm() {
+				return {
+					name: document.getElementById('name').value,
+					city: document.getElementById('city').value,
+					adress: document.getElementById('adress').value,
+					email: document.getElementById('email').value,
+					id_card: document.getElementById('id_card').value,
+				};
+			}
+			function setFormToContact(data){
+				document.getElementById('name').value = data.name;
+				document.getElementById('city').value = data.city;
+				document.getElementById('adress').value = data.adress;
+				document.getElementById('email').value = data.email;
+				document.getElementById('id_card').value = data.id_card;
+			}
+			  window.addEventListener('DOMContentLoaded', (event) => {
+			  var worker = new Worker('js/webworker.js');
+			  worker.onmessage = function(e) {
+				setFormToContact(e.data);
+			  }
+
+			  var worker_button = document.getElementById('worker_button');
+
+			  worker_button.addEventListener('click', function(e) {
+				console.log('Message sent to worker');
+				worker.postMessage(getContactFromForm());
+			  });
+			});
+		
+			const input = document.getElementById('imageLink');
+			const log = document.getElementById('imageShow');
+
+			input.addEventListener('change', updateValue);
+
+			function updateValue(e) {
+			  log.textContent = e.target.value;
+			  document.getElementById('imageShow').src = e.target.value;
+			}
